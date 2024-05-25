e4f218330c3b2d965f2eeabd0d2a41af195aadce
Kíron Okami <afk.kiron@gmail.com>
2024-05-25 12:57:31 -0300

    atividade familia

--- 04 - JavaScript/Aula 05 - DOM/Atividades/js/script.js
+++ 04 - JavaScript/Aula 05 - DOM/Atividades/js/script.js
@@ -1,5 +1,17 @@
 function calculo() {
     var filhos = document.querySelector("#filhos").value;
     var salario = document.querySelector("#salario").value;
+    if (salario <= 806.80) {
+        var cota = filhos * 41.37;
+    } else if (salario >= 806.81 && salario <= 1212.64) {
+        var cota = filhos * 29.16;
+    } else {
+        var cota = filhos * 0;
+    }
     
+    if (cota > 0) {
+        alert(`Seu salário família é R$${cota}!`)
+    } else {
+        alert(`Você não possui salário família!`)
+    }
 }
