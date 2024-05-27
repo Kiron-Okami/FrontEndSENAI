ddd53d2a7d40e7b58e3c715f3b8cf31edd906343
Kíron Okami <164359342+Kiron-Okami@users.noreply.github.com>
2024-05-27 14:30:21 -0300

    Update script.js

--- 04 - JavaScript/Aula 05 - DOM/Atividades/js/script.js
+++ 04 - JavaScript/Aula 05 - DOM/Atividades/js/script.js
@@ -1,6 +1,7 @@
+var empregado = document.querySelector("#empregado");
+var filhos = document.querySelector("#filhos").value;
+var salario = document.querySelector("#salario").value;
 function calculo() {
-    var filhos = document.querySelector("#filhos").value;
-    var salario = document.querySelector("#salario").value;
     if (salario <= 806.80) {
         var cota = filhos * 41.37;
     } else if (salario >= 806.81 && salario <= 1212.64) {
@@ -10,8 +11,8 @@ function calculo() {
     }
     
     if (cota > 0) {
-        alert(`Seu salário família é R$${cota}!`)
+        alert(`${empregado}, seu salário família é R$${cota}!`)
     } else {
-        alert(`Você não possui salário família!`)
+        alert(`${empregado}, você não possui salário família!`)
     }
 }
